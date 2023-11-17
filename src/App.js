9aa9bbc7ae5452272c533692b9f88f788c48ce47
Mohammed <elamrani.mohammed95@gmail.com>
2023-11-18 00:17:24 +0100

    cart

--- src/App.js
+++ src/App.js
@@ -10,122 +10,128 @@ import Kebab from "./kebab.jpg";
 import KFC from "./kfc.jpeg";
 import Sandwich from "./sandwich.jpg";
 
+// Composant BurgerBlock
+function BurgerBlock({ imagePath, burgerName, price, addToCart, removeFromCart}) {
+  const [qte, setQte] = useState(0);
 
-// Composant 
-function BurgerBlock({ imagePath, restaurantName, burgerName, price }) {
-
-    const [qte, setQte] = useState(0);
-
-    const incrementQte = () => {
-        setQte(qte + 1);
+  const incrementQte = () => {
+    setQte(qte + 1);
+    addToCart(1); 
+  };
+  const decrementQte = () => {
+    if (qte > 0) {
+      setQte(qte - 1);
+      removeFromCart(1); // 
     }
+  };
 
-    return (
-        <div className='shops--block'>
-            <img className='menu' src={imagePath} alt={restaurantName} />
-            <a href='#'>{restaurantName}</a>
-            <p className='block__desc'>{burgerName}</p>
-            <p className='block__prix'>{price}</p>
-            <button className='quantite' onClick={incrementQte}>
-                {qte}
-            </button>
+  return (
+    <div className='shops--block'>
+      <img href='#' className='menu' src={imagePath} alt={burgerName} />
+      <p className='block__desc'>{burgerName}</p>
+      <p className='block__prix'>{price}</p>
+        <div className='quantity-group'>
+            <button className='moin' onClick={decrementQte}>-</button>
+            <button className='quantite'>{qte}</button>
+            <button className='plus' onClick={incrementQte}>+</button>
         </div>
-    );
+    </div>
+  );
 }
 
 // Composant App
 function App() {
-    return (
-<>
-        <Monheader/>
-        <div className='App'>
-            {/* Votre contenu HTML */}
-            <header>
-                <div className='container'>
-                    <div className='header'>
-                        <div className='headerlogo'>
-                            <img
-                                class='logi'
-                                src={Logo}
-                                alt='Image du menu'
-                            />
-                        </div>
+  const [cart, setCart] = useState(0);
 
-                        <div className='header--address'>
-                            <input
-                                type='text'
-                                name=''
-                                id=''
-                                placeholder='*****'
-                            />
-                            <p>to</p>
-                            <input
-                                type='text'
-                                name=''
-                                id=''
-                                placeholder='******'
-                            />
-                        </div>
+  const addToCart = (quantity) => {
+    setCart(cart + quantity);
+  };
 
-                        <div className='header--buttons'>
-                            <div>
-                                <a href=''>Sign In</a>
-                                <a href=''>Register</a>
-                            </div>
-                            <div className='header--cart'>
-                                <svg
-                                    viewBox='0 0 40 72'
-                                    fill='none'
-                                    xmlns='http://www.w3.org/2000/svg'>
-                                    {/* ... (votre contenu SVG) */}
-                                </svg>
-                            </div>
-                        </div>
-                    </div>
+  const removeFromCart = (quantity) => {
+    setCart(cart - quantity);
+  };
+
+  return (
+    <>
+      <Monheader />
+      <div className='App'>
+        <header>
+          <div className='container'>
+            <div className='header'>
+              <div className='headerlogo'>
+                <img className='logi' src={Logo} alt='Image du menu' />
+              </div>
+
+              <div className='header--address'>
+                <input type='text' name='' id='' placeholder='*****' />
+                <p>to</p>
+                <input type='text' name='' id='' placeholder='******' />
+              </div>
+
+              <div className='header--buttons'>
+                <div>
+                  <a href='#'>Sign In</a>
+                  <a href='#'>Register</a>
                 </div>
-            </header>
-
-            <section className='shops'>
-                <div className='container'>
-                    <div className='shops--title'>
-                        <h1>Offres du jour</h1>
-                    </div>
-
-                    <div className='shopsblocks'>
-
-                        <BurgerBlock
-                            imagePath={Macdo}
-                            burgerName='Menu MacDo™'
-                            price='12,50 €'
-                             
-                        />
-                        <BurgerBlock
-                            imagePath={Kebab}
-                            burgerName='Kebab™'
-                            price='8,50 €'
-                          
-                        />
-                        
-                            <BurgerBlock
-                                imagePath={KFC}
-                                burgerName='KFC™'
-                                price='10,00 €'
-                                
-                            />
-                       
-                            <BurgerBlock
-                                imagePath={Sandwich}
-                                burgerName='Sandwich™'
-                                price='3,50 €'
-                            
-                            />
-                        
-                    </div>
+                <div class='cart-icon'>
+                  <div>
+                    <iconify-icon icon='ion:cart' width='30' height='30'></iconify-icon>
+                  </div>
+                  <div id='cart' class='bubble'>
+                    {cart}
+                  </div>
                 </div>
-            </section>
-        </div>
-        </>
-    );
+                <div className='header--cart'>
+                  <svg viewBox='0 0 40 72' fill='none' xmlns='http://www.w3.org/2000/svg'>
+                    {}
+                  </svg>
+                </div>
+              </div>
+            </div>
+          </div>
+        </header>
+
+        <section className='shops'>
+          <div className='container'>
+            <div className='shops--title'>
+              <h1>Offres du jour</h1>
+            </div>
+
+            <div className='shopsblocks'>
+              <BurgerBlock
+                imagePath={Macdo}
+                burgerName='Menu MacDo™'
+                price='12,50 €'
+                addToCart={addToCart}
+                removeFromCart={removeFromCart}
+              />
+              <BurgerBlock
+                imagePath={Kebab}
+                burgerName='Kebab™'
+                price='8,50 €'
+                addToCart={addToCart}
+                removeFromCart={removeFromCart}
+              />
+              <BurgerBlock
+                imagePath={KFC}
+                burgerName='KFC™'
+                price='10,00 €'
+                addToCart={addToCart}
+                removeFromCart={removeFromCart}
+              />
+              <BurgerBlock
+                imagePath={Sandwich}
+                burgerName='Sandwich™'
+                price='3,50 €'
+                addToCart={addToCart}
+                removeFromCart={removeFromCart}
+              />
+            </div>
+          </div>
+        </section>
+      </div>
+    </>
+  );
 }
 
 export default App;
